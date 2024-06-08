a1c27d7ca1f6cc443a6f8f2834c6ee31bba97427
Julien Collas <jul.collas@gmail.com>
2024-06-08 14:44:25 +0200

    change vigicrues api url

--- custom_components/vigicrues/const.py
+++ custom_components/vigicrues/const.py
@@ -1,7 +1,7 @@
 from homeassistant.const import LENGTH_METERS, VOLUME_CUBIC_METERS
 
 CONF_STATIONS = "stations"
-VIGICRUES_URL = "https://www.vigicrues.gouv.fr/services/observations.json"
+VIGICRUES_URL = "https://www.vigicrues.gouv.fr/services/observations.json/index.php"
 
 METRICS_INFO = {
     "H": {"name": "Hauteur", "unit": LENGTH_METERS},
